c81643d7b3a47d7dc9c120452473e6d5e3ad3c8d
yotaseki <uyu9480s@gmail.com>
2017-12-05 11:25:45 +0900

    modified figure

--- SimpleRobotApproach-PolicyGradient/main.m
+++ SimpleRobotApproach-PolicyGradient/main.m
@@ -1,8 +1,8 @@
 L = 20;
 M = 2000;
 T = 20;
-gamma = 0.99;
-alpha = 1;
+gamma = 0.98;
+alpha = 0.9;
 
 N = 2; %(mu:1ŽŸŒ³, sigma:1ŽŸŒ³)
 
--- SimpleRobotApproach-PolicyGradient/plotSimulation.m
+++ SimpleRobotApproach-PolicyGradient/plotSimulation.m
@@ -1,4 +1,4 @@
-function plotSimulation(goalpos, robotpos, robotang, Title) % goal_pos,state,actions(action)
+function plotSimulation(goalpos, robotpos, robotangle, Title) % goal_pos,robotangle,actions(action)
 
 figure(1);
 clf;
@@ -13,21 +13,22 @@ xlim([-0.8 0.8]);
 ylim([-0.3 1.3]);
 axis square;
 
-% ñ⁄ìIínÇÃï`âÊ
+% ÔøΩ⁄ìIÔøΩnÔøΩÃï`ÔøΩÔøΩ
 viscircles([goalpos(1) goalpos(2)],0.03);
-%text(goal(1)+0.1,goal(2),strcat(num2str(rad2deg(goal(3))),'Åã'));
+%text(goal(1)+0.1,goal(2),strcat(num2str(rad2deg(goal(3))),'ÔøΩÔøΩ'));
 %c_goal = [goal(1) goal(2)];
 %g_goal = [goal(1) + cos(goal(3)) goal(2) + sin(goal(3))];
 %line_goal = [c_goal;g_goal];
 %plot(line_goal([1,2],1),line_goal([1,2],2),'-','Color','r');
 
+relative_ang = getRobotState(atan2(goalpos(2),goalpos(1)),robotangle);
 
-% ÉçÉ{ÉbÉgÇÃï`âÊ
+% ÔøΩÔøΩÔøΩ{ÔøΩbÔøΩgÔøΩÃï`ÔøΩÔøΩ
 r_radii = 0.04;
 viscircles([robotpos(1) robotpos(2)],r_radii,'Color','k');
-text(robotpos(1)+0.1,robotpos(2),strcat(num2str(rad2deg(robotang)),'Åã'));
+text(robotpos(1)+0.1,robotpos(2),strcat(num2str(rad2deg(relative_ang)),'[deg]'));
 c_robot = [robotpos(1) robotpos(2)];
-g_robot = [robotpos(1) + cos(robotang) robotpos(2) + sin(robotang)];
+g_robot = [robotpos(1) + cos(robotangle) robotpos(2) + sin(robotangle)];
 %plot(a([c_robot(1) g_robot(1)],1),a([c_robot(2) g_robot(2)],2),'-')
 line_robot = [c_robot;g_robot];
 plot(line_robot([1,2],1),line_robot([1,2],2),'-','Color','k');
@@ -35,5 +36,5 @@ plot(line_robot([1,2],1),line_robot([1,2],2),'-','Color','k');
 %saveas(field, 'simpleSim.jpg');
 
 grid on;
-pause(0.1);
+pause(0.2);
 end
